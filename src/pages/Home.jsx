cf9608e130778e280ab5070284ba57f846107f1d
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2024-10-09 17:20:16 +0530

    NavBar, Catalog, and apiConnector codes added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -198,6 +198,7 @@ const Home = () => {
 
             <h2 className='text-center text-4xl font-semobold mt-10'>review from Other Learners</h2>
             {/* Review Slider here */}
+            
       </div>
 
 
