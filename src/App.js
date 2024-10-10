ae8d3c126d9e4d942cecd1f230dd629f11533586
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2024-10-10 23:48:40 +0530

    MyProfile page, and dashboard code added with backend

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Error from "./pages/Error"
 
 function App() {
   return (
@@ -68,7 +73,24 @@ function App() {
               <About />
             </OpenRoute>
           }
-        />  
+        />
+    <Route path="/contact" element={<Contact />} />
+
+    <Route 
+      element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      }
+    >
+      <Route path="dashboard/my-profile" element={<MyProfile />} />
+      {/* <Route path="dashboard/settings" element={<Setting />} /> */}
+
+    </Route>
+
+    
+
+    <Route path="*" element={<Error />} />
 
 
     </Routes>
