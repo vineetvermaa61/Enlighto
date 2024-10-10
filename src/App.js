53a3cdb07e9699ad53b87296325ebbe2fba4e87d
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2024-10-10 18:00:07 +0530

    Authorization, AboutUs and ContactUs pages added with backend

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import OpenRoute from "./components/core/Auth/OpenRoute"
 
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -29,6 +33,44 @@ function App() {
             </OpenRoute>
           }
         />
+
+    <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />  
+
+      <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />  
+
+    <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />  
+
+    <Route
+          path="about"
+          element={
+            <OpenRoute>
+              <About />
+            </OpenRoute>
+          }
+        />  
+
+
     </Routes>
 
    </div>
