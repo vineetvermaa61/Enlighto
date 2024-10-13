98e2cb34abf63af0e7a62ee72ef868cb116b8775
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2024-10-13 11:35:26 +0530

    Add to Cart, My Course page codes with backend added

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL)
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    })
     .then(() => console.log("DB Connected Successfully"))
     .catch( (error) => {
         console.log("DB Connection Failed");
--- src/components/core/Catalog/CourseSlider.jsx
+++ src/components/core/Catalog/CourseSlider.jsx
@@ -4,7 +4,7 @@ import {Swiper, SwiperSlide} from "swiper/react"
 import "swiper/css"
 import "swiper/css/free-mode"
 import "swiper/css/pagination"
-import { FreeMode, Pagination } from 'swiper/modules';
+import { FreeMode, Pagination}  from 'swiper/modules'
 
 import Course_Card from './Course_Card'
 
