9e6011baf02b261fdbe6e7ec16aafd670bdef408
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2024-10-15 02:27:13 +0530

    deployment bug5 fixed

--- server/routes/Course.js
+++ server/routes/Course.js
@@ -36,7 +36,7 @@ const {
   createSubSection,
   updateSubSection,
   deleteSubSection,
-} = require("../controllers/SubSection")
+} = require("../controllers/Subsection")
 
 // Rating Controllers Import
 const {
